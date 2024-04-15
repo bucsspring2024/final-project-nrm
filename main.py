b05a8e234ce1abda472a95f79e5eafdf42f34c6c
Your Name <you@example.com>
2024-04-15 17:16:40 -0400

    2

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pygame
 pygame.init()
-screen = pygame.display.set_mode()
-width, height = pygame.display.get_window_size()
+screen = pygame.display.set_mode(640,480)
+screen.fill("blue")
+font = pygame.font.Font(None, 36)
+text = font.render("Welcome to the Pizza shop!", True, "white")
+text_rect = text.get_rect(640/2,480/2)
+screen.blit(text, text_rect)
+pygame.display.flip()
+pygame.time.wait(3000)
 
 
 def main():
