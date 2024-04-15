a636ecb49f25b6915e057bceda9f678e94d11d4f
Your Name <you@example.com>
2024-04-15 16:07:14 -0400

    update

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode()
 width, height = pygame.display.get_window_size()
-screen.fill("blue")
 
+
+class user:
+  
+class hair_style:
+  def __init__(self, name, color, length, thickness):
+    self.name = name
+    self.color = color
+    self.length = length
+    
