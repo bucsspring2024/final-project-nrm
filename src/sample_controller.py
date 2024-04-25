f4299bc4a1ce65d1a2d3a742ee5bfe9bcd376809
Your Name <you@example.com>
2024-04-25 13:34:52 -0400

    2

--- src/sample_controller.py
+++ src/sample_controller.py
@@ -1,33 +1,38 @@
-
-class Controller:
-  
-  def __init__(self):
-    #setup pygame data
-    
-  def mainloop(self):
-    #select state loop
-    
-  
-  ### below are some sample loop states ###
-
-  def menuloop(self):
-    
-      #event loop
-
-      #update data
-
-      #redraw
-      
-  def gameloop(self):
-      #event loop
-
-      #update data
-
-      #redraw
-    
-  def gameoverloop(self):
-      #event loop
-
-      #update data
-
-      #redraw
+#  importing required library
+import pygame
+ 
+# activate the pygame library .
+pygame.init()
+X = 600
+Y = 600
+ 
+# create the display surface object
+# of specific dimension..e(X, Y).
+scrn = pygame.display.set_mode((X, Y))
+ 
+# set the pygame window name
+pygame.display.set_caption('image')
+ 
+# create a surface object, image is drawn on it.
+imp = pygame.image.load("assets/school-teacher-blackboard-classroom-cartoon-teacher.jpg").convert()
+ 
+# Using blit to copy content from one surface to other
+scrn.blit(imp, (0, 0))
+ 
+# paint screen one time
+pygame.display.flip()
+status = True
+while (status):
+ 
+  # iterate over the list of Event objects
+  # that was returned by pygame.event.get() method.
+    for i in pygame.event.get():
+ 
+        # if event object type is QUIT
+        # then quitting the pygame
+        # and program both.
+        if i.type == pygame.QUIT:
+            status = False
+ 
+# deactivates the pygame library
+pygame.quit()
